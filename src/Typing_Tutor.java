f29e0814eea54700c448bcb0f5474e072f6de8e6
trygamer <trygamer777@gmail.com>
2018-06-04 20:57:51 -0700

    Typing_Tutor.ip

--- src/Typing_Tutor.java
+++ src/Typing_Tutor.java
@@ -1,20 +1,42 @@
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Typing_Tutor {
 	JFrame f = new JFrame();
 	JPanel p = new JPanel();
+	JLabel l = new JLabel();
+	char currentLetter= generateRandomLetter();
 
+	
+	
+	
+	
 	public static void main(String[] args) {
 
+		new Typing_Tutor().Set();
+		
 	}
 
-	public void Set() {
-		f.setTitle("Type or Die");
+	public  void Set() {
+		
 		f.setVisible(true);
+		f.setTitle("Type or Die");
 		f.add(p);
+		p.add(l);
+		l.setText(System.out.print(currentLetter));
 		f.pack();
+		
 
 	}
 
+ ;
+	
+	
+	char generateRandomLetter() {
+		 Random r = new Random();
+	      return (char) (r.nextInt(26) + 'a');
+}
 }
