9c460820a207a8042a9c8b3f2a9ce80726c2b65f
Lucas B <league@WTS5.attlocal.net>
2018-06-29 18:29:46 -0700

    Netflix.ip

--- src/NetflixQueue.java
+++ src/NetflixQueue.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -6,7 +7,36 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.JOptionPane;
+
 public class NetflixQueue {
+
+	public static void main(String[] args) {
+		
+		NetflixQueue nq = new NetflixQueue();
+		String mov2 = "Jurassic World";
+		String mov3 = "SlenderMan";
+
+		String mov4 = "Robin Hood";
+		String mov5 = "The Lego Movie";
+		Movie m = new Movie("Boss Baby", 2);
+		Movie m1 = new Movie(mov2, 4);
+		Movie m2 = new Movie(mov3, 5);
+		Movie m3 = new Movie(mov4, 1);
+		Movie m4 = new Movie(mov5, 3);
+nq.addMovie(m);
+nq.addMovie(m1);
+nq.addMovie(m2);
+nq.addMovie(m3);
+nq.addMovie(m4);
+m1.getTicketPrice();
+ Movie best = nq.getBestMovie();
+ JOptionPane.showMessageDialog(null, "The best movie is "+ best );
+ JOptionPane.showMessageDialog(null, "The best movie is "+ best );
+
+
+nq.sortMoviesByRating();
+	}
 
 	ArrayList<Movie> movies = new ArrayList<Movie>();
 
--- src/Slot_Machine.java
+++ src/Slot_Machine.java
@@ -8,6 +8,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Slot_Machine implements ActionListener {
@@ -23,8 +24,8 @@ public class Slot_Machine implements ActionListener {
 	JLabel l3 = new JLabel();
 
 	String Slot1 = "lemon.jpg";
-	String Slot2 = "/level1-module3-trygamer/src/cherry.jpg";
-	String Slot3 = "/level1-module3-trygamer/src/ bell.jpg";
+	String Slot2 = "cherry.jpg";
+	String Slot3 = " bell.jpg";
 
 	public static void main(String[] args) {
 		new Slot_Machine().Set();
@@ -33,15 +34,16 @@ public class Slot_Machine implements ActionListener {
 	public void Set() {
 
 		f.setVisible(true);
-		f.add(p);
-		p.add(l);
-		p.add(l2);
-		p.add(l3);
+		// f.add(p);
+		f.getContentPane().add(p);
+
 		p.add(b1);
 
 		b1.setText("SPIN");
 		b1.addActionListener(this);
 		f.pack();
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public JLabel loadImageFromComputer(String fileName) {
@@ -57,29 +59,41 @@ public class Slot_Machine implements ActionListener {
 		int v = y.nextInt(3);
 		int b = y.nextInt(3);
 
-		l = loadImageFromComputer(Slot1);
-		// if (r == 1) {
-		// l.add(loadImageFromComputer(Slot1));
-		// } else if (r == 2) {
-		// l.add(loadImageFromComputer(Slot2));
-		// } else if (r == 0) {
-		// l.add(loadImageFromComputer(Slot3));
-		// }
-		// if (v == 1) {
-		// l2.add(loadImageFromComputer(Slot1));
-		// } else if (v == 2) {
-		// l2.add(loadImageFromComputer(Slot2));
-		// } else if (v == 0) {
-		// l2.add(loadImageFromComputer(Slot3));
-		// }
-		// if (b == 1) {
-		// l3.add(loadImageFromComputer(Slot1));
-		// } else if (b == 2) {
-		// l3.add(loadImageFromComputer(Slot2));
-		// } else if (b == 0) {
-		// l3.add(loadImageFromComputer(Slot3));
-		// }
+		p.remove(l);
+		p.remove(l2);
+		p.remove(l3);
+
+		if (r == 1) {
+			l = loadImageFromComputer(Slot1);
+		} else if (r == 2) {
+			l = loadImageFromComputer(Slot2);
+		} else if (r == 0) {
+			l = loadImageFromComputer(Slot3);
+		}
+		if (v == 1) {
+			l2 = loadImageFromComputer(Slot1);
+		} else if (v == 2) {
+			l2 = loadImageFromComputer(Slot2);
+		} else if (v == 0) {
+			l2 = loadImageFromComputer(Slot3);
+		}
+		if (b == 1) {
+			l3 = loadImageFromComputer(Slot1);
+		} else if (b == 2) {
+			l3 = loadImageFromComputer(Slot2);
+		} else if (b == 0) {
+			l3 = loadImageFromComputer(Slot3);
+		}
+		p.add(l);
+		p.add(l2);
+		p.add(l3);
 		f.pack();
+
+		if (r == v && r == b && v == b) {
+
+			JOptionPane.showMessageDialog(null, "You Won!");
+		}
+
 	}
 
 }
